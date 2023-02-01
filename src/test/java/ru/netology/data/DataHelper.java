24409459ab2c33102f418709edae8a66179a0af2
Anna <106378899+Malikosss@users.noreply.github.com>
2023-02-01 16:23:57 +0700

    исправление в тесте

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -14,7 +14,7 @@ public class DataHelper {
     }
 
     public static AuthInfo getAuthInfo() {
-        return new AuthInfo("Anna", "qwerty123");
+        return new AuthInfo("vasya", "qwerty123");
     }
 
     @Value
